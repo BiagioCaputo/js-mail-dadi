be8bd33f8cbd68c5e86b7b8bfc034d72392e34fd
BiagioCaputo <scebiagio98@gmail.com>
2023-12-21 15:58:30 +0100

    2 esercizio terminato

--- js/script.js
+++ js/script.js
@@ -39,3 +39,43 @@ resultDicePc.innerText = randomNumberPc;
 
 resultRollDices.innerText = finalResult;
 
+// ESERCIZIO 2
+
+// creo le mie variabili e il mio array
+
+const emailsAuthorized = [
+    "ioposso@sium.lol",
+    "iopossoancora@sium.lol",
+    "iopossoeriposso@sium.lol",
+    "iopossoetuno@sium.lol",
+    "iopossoeccome@sium.lol",
+]
+
+const userEmail = document.getElementById("user-email");
+const checkButton = document.getElementById("check-button");
+const permissionMessage = document.getElementById("permission");
+
+let valid = 'false'; // variabile per la validazione
+
+//metto il button in ascolto
+checkButton.addEventListener('click', function (){
+    
+    // controllo ogni elemento dell'array
+    for(let i = 0; i < emailsAuthorized.length; i++){
+
+        // Vedo se l'email è uguale ad un elemento dell'array
+        if(emailsAuthorized[i] === userEmail.value){
+            valid = "true";
+        }
+
+    }
+    
+    //stampo il risultato differente in base al valore della variabile di validazione
+    if(valid === "true"){
+        permissionMessage.innerText = "Sei autorizzato a procedere oltre";
+    }
+
+    else{
+        permissionMessage.innerText = "Non sei autorizzato a procedere oltre, la tua email non è tra quelle selezionate";
+    }
+})
